c5bc1775292d26f80b67df1372acbe2b23e158c9
Mark Kravchenko <mark.kravchenko@refinitiv.com>
2021-08-31 10:36:07 +0300

    Add project files.

---
+++ NetFrameworkTestApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrameworkTestApp
+{
+    using System.Runtime.Remoting.Contexts;
+    using System.Threading;
+
+    class Program
+    {
+        public delegate int BinaryOp(int x, int y);
+
+        static void Main(string[] args)
+        {
+            BinaryOp delBinaryOp = new BinaryOp(Add);
+
+            Console.WriteLine("answer: {0}", delBinaryOp.Invoke(5, 4));
+
+            Console.ReadKey();
+        }
+
+        static int Add(int x, int y)
+        {
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
